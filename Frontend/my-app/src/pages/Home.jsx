a40db044791103ff72e6456c0000bd1229e86967
Marx <marxborgesmachado@gmail.com>
2021-07-21 19:40:04 -0300

    Frontend 2/3

--- Frontend/my-app/src/pages/Home.jsx
+++ Frontend/my-app/src/pages/Home.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import '../styles/global.css';
-import '../styles/home.css'
+import '../styles/home.css';
+import img from '../images/Danone.jpg';
 
 function Home(){
     return(
         <div className="App">
             <h1 className="Title">Convenience Store</h1>
+            <h2 className="Add">Add Food</h2>
+            <ul className="ShowItens">
+                <li className="Item">
+                    <h3>Danone</h3>
+                    <img src={img} alt="" className="Image"/>
+                    <h3>Amount: 4875</h3>
+                </li>
+                <li className="Item">
+                    <h3>Danone</h3>
+                    <img src={img} alt="" className="Image"/>
+                    <h3>Amount: 4875</h3>
+                </li>
+                <li className="Item">
+                    <h3>Danone</h3>
+                    <img src={img} alt="" className="Image"/>
+                    <h3>Amount: 4875</h3>
+                </li>
+                <li className="Item">
+                    <h3>Danone</h3>
+                    <img src={img} alt="" className="Image"/>
+                    <h3>Amount: 4875</h3>
+                </li>
+            </ul>
         </div>
     )
 }
